3d016ccac7a9c50e283d4c7e7761883c02e9c2db
Dex Fitch <fitch.dexter@gmail.com>
2021-10-27 07:48:45 -0700

    Update link for OAuth tokens (#1)
    * Updates the link where students can find their OAuth token now that learn.co is sunset
    
    * Improve descriptive paragraph before link
    
    * Remove 'here' from description of link
    
    * Update copy
    
    Co-authored-by: drew <drewprice@users.noreply.github.com>

--- lib/learn_config/cli.rb
+++ lib/learn_config/cli.rb
@@ -10,9 +10,9 @@ module LearnConfig
     def ask_for_oauth_token(short_text: false, retries_remaining: 5)
       if !short_text
         puts <<-LONG
-To connect with the Learn web application, you will need to configure
-the Learn gem with an OAuth token. You can find yours at the bottom of your profile
-page at: https://learn.co/#{github_username ? github_username : 'your-github-username'}.
+In order to receive credit for your work, you will need to configure
+the Learn gem with an OAuth token. You can find yours in Base at:
+  https://base.flatironschool.com/account/manage
 
         LONG
 
